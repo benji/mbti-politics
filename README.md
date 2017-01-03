a264980dd53a30ac6d154910d17969a092ed15c9
Benjamin Jaton <benjamin.jaton@gmail.com>
2017-01-03 01:01:42 -0800

    First draft

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Principal Component Analysis to correlate political affiliation with MBTI types.
 
 Data from:
 ![](https://github.com/benji/mbti-politics/blob/master/data_mbti_political_affiliation.png?raw=true)
+
+Type frequency from:
+http://www.myersbriggs.org/my-mbti-personality-type/my-mbti-results/how-frequent-is-my-type.htm
+
+TODO:
+- display types next to symbol
+- color intensity?
+- 
--- data.js
+++ data.js
@@ -1,21 +1,21 @@
 var mbti_politics = [
-  {name: 'ISTJ', dem: 30, rep: 42},
-  {name: 'ISFJ', dem: 34, rep: 36},
-  {name: 'INFJ', dem: 49, rep: 22},
-  {name: 'INTJ', dem: 19, rep: 40},
+  {name: 'ISTJ', dem: 30, rep: 42, freq: 11.6},
+  {name: 'ISFJ', dem: 34, rep: 36, freq: 13.8},
+  {name: 'INFJ', dem: 49, rep: 22, freq: 1.5},
+  {name: 'INTJ', dem: 19, rep: 40, freq: 2.1},
   
-  {name: 'ISTP', dem: 28, rep: 38},
-  {name: 'ISFP', dem: 33, rep: 26},
-  {name: 'INFP', dem: 38, rep: 22},
-  {name: 'INTP', dem: 17, rep: 34},
+  {name: 'ISTP', dem: 28, rep: 38, freq: 5.4},
+  {name: 'ISFP', dem: 33, rep: 26, freq: 8.8},
+  {name: 'INFP', dem: 38, rep: 22, freq: 4.4},
+  {name: 'INTP', dem: 17, rep: 34, freq: 3.3},
   
-  {name: 'ESTP', dem: 27, rep: 35},
-  {name: 'ESFP', dem: 39, rep: 31},
-  {name: 'ENFP', dem: 34, rep: 31},
-  {name: 'ENTP', dem: 26, rep: 28},
+  {name: 'ESTP', dem: 27, rep: 35, freq: 4.3},
+  {name: 'ESFP', dem: 39, rep: 31, freq: 8.5},
+  {name: 'ENFP', dem: 34, rep: 31, freq: 8.1},
+  {name: 'ENTP', dem: 26, rep: 28, freq: 3.2},
   
-  {name: 'ESTJ', dem: 32, rep: 46},
-  {name: 'ESFJ', dem: 33, rep: 37},
-  {name: 'ENFJ', dem: 35, rep: 35},
-  {name: 'ENTJ', dem: 26, rep: 40}
+  {name: 'ESTJ', dem: 32, rep: 46, freq: 8.7},
+  {name: 'ESFJ', dem: 33, rep: 37, freq: 12.3},
+  {name: 'ENFJ', dem: 35, rep: 35, freq: 2.5},
+  {name: 'ENTJ', dem: 26, rep: 40, freq: 1.8}
 ]
